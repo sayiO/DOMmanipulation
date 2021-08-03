2ba1a539b9a0452d0b866c1f935fbf1c587ec824
sayiO <sayionias@gmail.com>
2021-08-03 22:45:57 +0200

    adding the eventListeners

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function firstClick() {
     alert("you clicked the first button");
 
 }
+
+let secondBtn = document.querySelector(".second_button");
+secondBtn.onclick = function() {
+    alert("i have clicked the second button");
+}
