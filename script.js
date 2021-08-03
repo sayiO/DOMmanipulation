213c7a74ecb36cbb8e86a10fd85a46b204ee6068
sayiO <sayionias@gmail.com>
2021-08-04 00:02:18 +0200

    local storage and session storage

--- script.js
+++ script.js
@@ -20,5 +20,12 @@ let listItems = document.querySelectorAll("li");
 for (let i = 0; i < listItems.length; i++) {
     listItems[i].addEventListener("click", function(event) {
         alert("you have selected the item " + event.target.innerText);
-    });
-}
+    })
+
+    function showLocalStorage() {
+        let instructors = ["Elie", "Matt", "Tim"];
+
+        localStorage.setItem("instructors", JSON.stringfy(instructors));
+        JSON.parse(localStorage.getItem(instructors));
+
+    }
