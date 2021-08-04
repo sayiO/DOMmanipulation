46cb78983409e0dfd437938115bf52b2ddac1280
sayiO <sayionias@gmail.com>
2021-08-04 13:31:33 +0200

    changed the multiple elements properties using event listeners

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.addEventListener("DOMContentLoaded", function() {
 //2.changing text of an element
 let h1_text = document.getElementsByTagName("h1")[0];
 h1_text.innerText = "Hello World!";
+
+//3.changing multiple elements.
+
+
+
+document.addEventListener("click", function() {
+    let divs = document.getElementsByTagName("div");
+
+    for (let i = 0; i < 4; i++) {
+        divs[i].innerText = divs[i].className;
+    }
+
+})
