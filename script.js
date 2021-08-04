fa664240a2a1dac172a51c8eca00a947674ce6a5
sayiO <sayionias@gmail.com>
2021-08-04 12:51:51 +0200

    changed elememnt's text using DOM manipulation techniques

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
-let btns = document.getElementsByTagName("button");
+//1.
+document.addEventListener("DOMContentLoaded", function() {
+    //alert("dom has loaded");
+});
 
-function alertData() {
-    alert("bithch you can be good at clicking them buttons if you believe bitch....");
-
-}
-
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", alertData);
-}
-
-for (let i = 0; i < btns.length; i++) {
-    btns[i].removeEventListener("click", alertData);
-}
-
-let listItems = document.querySelectorAll("li");
-
-
-
-for (let i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener("click", function(event) {
-        alert("you have selected the item " + event.target.innerText);
-    })
-
-    function showLocalStorage() {
-        let instructors = ["Elie", "Matt", "Tim"];
-
-        localStorage.setItem("instructors", JSON.stringfy(instructors));
-        JSON.parse(localStorage.getItem(instructors));
-
-    }
+//2.changing text of an element
+let h1_text = document.getElementsByTagName("h1")[0];
+h1_text.innerText = "Hello World!";
