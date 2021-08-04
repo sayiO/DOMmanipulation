7144dbd8754ff9c6b93ed190f83da0bd7e024358
sayiO <sayionias@gmail.com>
2021-08-04 16:25:15 +0200

    created, styled and appended an element to the DOM

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 //1.
-document.addEventListener("DOMContentLoaded", function() {
-    //alert("dom has loaded");
-});
+window.onload = function() {}
 
 //2.changing text of an element
-let h1_text = document.getElementsByTagName("h1")[0];
-h1_text.innerText = "Hello World!";
+window.onload = function() {
+    var h1_text = document.getElementById("change_heading");
+    h1_text.innerText = "Hello World!";
+}
 
 //3.changing multiple elements.
+window.onload = function() {
+    var h1_text = document.getElementById("change_heading");
+    h1_text.innerText = "Hello World!";
+
+    let section = document.querySelector("section");
+    section.addEventListener("mouseover", function(event) {
+        let selectedColor = document.querySelector(".selected");
+        selectedColor.innerText = event.target.className;
+    })
+}
 
 
 
-document.addEventListener("click", function() {
-    let divs = document.getElementsByTagName("div");
 
-    for (let i = 0; i < 4; i++) {
-        divs[i].innerText = divs[i].className;
-    }
-
-})
 
 //4. creating new elements
 
@@ -27,7 +30,7 @@ let newDiv = document.createElement("div");
 //5.styling the element
 
 newDiv.className = "purple";
-newDiv.style.background = "purple";
+//newDiv.style.background = "purple";
 
 //6. appending new elements to the DOM
 
