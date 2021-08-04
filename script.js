a3354d4967cf06199d3be82dd6f94d6a88adeb29
sayiO <sayionias@gmail.com>
2021-08-04 13:45:41 +0200

    created, styled and appended an element to the DOM

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ document.addEventListener("click", function() {
     }
 
 })
+
+//4. creating new elements
+
+let newDiv = document.createElement("div");
+
+//5.styling the element
+
+newDiv.className = "purple";
+newDiv.style.background = "purple";
+
+//6. appending new elements to the DOM
+
+let section = document.querySelector("section");
+section.appendChild(newDiv);
