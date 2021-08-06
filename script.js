c9ed3a216cea5c3bd4685fcadaa241b9888f74c9
sayiO <sayionias@gmail.com>
2021-08-06 12:14:48 +0200

    making the todo application

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ window.onload = function addListItems() {
     let taskName = document.getElementById("todo");
 
 }
+
+let myListItems = document.getElementsByTagName("li");
+for (let i = 0; i < myListItems.length; i++) {
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myListItems[i].appendChild(span);
+}
+
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+    }
+}
