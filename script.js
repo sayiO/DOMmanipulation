d8e528b22609b2bfc9579ed654e0b7dfa50f2120
sayiO <sayionias@gmail.com>
2021-08-07 17:26:25 +0200

    function to get the first and last values of an array value

--- script.js
+++ script.js
@@ -41,3 +41,43 @@ function printFirstAndLast(arr) {
 
 
 }
+
+//getting element's double values using forEach iterator
+
+function double(arr) {
+    let doubledArr = [];
+    arr.forEach(function(val) {
+        doubledArr.push(val * 2);
+    })
+    return doubledArr;
+}
+
+// adding key and values to an array of objects using forEach iterator
+
+function addKeyAndValue(arr, key, value) {
+    let newObj = [];
+    arr.forEach(function(arr) {
+        arr[key] = "value";
+        newObj.push(arr);
+    })
+    return newObj;
+
+}
+
+//using a map function 
+
+function valTimesIndex(arr) {
+    return arr.map(function(val, index) {
+        return val * index;
+    });
+
+}
+
+//returning doubles values using map
+function doubleArray(arr) {
+    // return the result of arr.map
+    return arr.map(function(val) {
+        // return a new array with each value doubled
+        return val * 2;
+    });
+}
