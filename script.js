e4fe17c157ba3683207424a27b6aa210b78f9932
sayiO <sayionias@gmail.com>
2021-08-14 19:33:07 +0200

    creating and adding elements to the DOM using jquery function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("article").addClass("hidden");
     $("article").removeClass("hidden");
     $("article").toggleClass("hidden");
-    $("li").text("madii cde");
+    //$("li").text("madii cde");
     $("input").val("this is  the new value");
 
 
@@ -11,4 +11,28 @@ $(document).ready(function() {
 
     $("article").css("background-color", "teal");
     $("article").css("style", "display:flex;");
+
+    //DOM traversal........
+    let childElements = $("article").find("div").children();
+    let el = $("article").find("div").children();
+
+    //creating and appending elements to the dom
+    let newP = $("<p>");
+    newP.text("I am the new king in town gentlemen");
+    newP.css("color", "red");
+    $("article").append(newP);
+
+    let anotherDiv = $("<p>", {
+        text: "this is a latest div gentlemen",
+        css: {
+            color: "green",
+            background: "pink",
+            "font-size": "4em"
+
+        }
+    });
+
+    $("article").append(anotherDiv);
+
+
 })
