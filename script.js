b43b0ea752d67e4980e2317060be7d0ebbc5e1a5
sayiO <sayionias@gmail.com>
2021-08-14 11:22:33 +0200

    changing dom styles using jquery function

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
     $("article").toggleClass("hidden");
     $("li").text("madii cde");
     $("input").val("this is  the new value");
+
+
+
+    //accessing elements
+
+    $("article").css("background-color", "teal");
+    $("article").css("style", "display:flex;");
 })
