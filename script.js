4c6027bcc09a0415d8b518e1922f706fe5798687
sayiO <sayionias@gmail.com>
2021-08-14 10:36:32 +0200

    changing the values of the dom elements using jquery setters

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 $(document).ready(function() {
-
-
-
-    //selecting footer element
-
-    let footer = $("footer");
-    //select div with id container
-
-    let div = $("#container");
-    //listitem inside nav class
-    let listItems = $(".nav li");
-
-    //third list item inside div with class list-container
-
-    let listItems2 = $(".list-container li:nth-child(3)");
-
-
-    //select only last list item in ul
-
-    lastItem = $("ul li:last-child")
-
-
-
-
-
-});
+    $("article").html();
+    $("li").text("madii cde");
+    $("input").val("this is  the new value");
+})
